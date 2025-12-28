ecd9abbd425bd49646b88c5356a557616e031c89
Nelani Maluka <malukanelani@gmail.com>
2025-12-28 10:13:54 +0200

    first commit

---
+++ src/test/java/com/nelani/demo/PaymentGatewayIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelani.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
